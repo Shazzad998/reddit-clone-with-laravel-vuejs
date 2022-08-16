51e9e892350fdb8d96315e13422826582a78ca50
Md Shazzad <shazzadulislam998@gmail.com>
2022-08-16 18:20:40 +0600

    created model, migrations, commuinity controller and routes

--- routes/web.php
+++ routes/web.php
@@ -1,19 +1,10 @@
 <?php
 
+use App\Http\Controllers\Backend\CommunityController;
 use Illuminate\Foundation\Application;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
 
 Route::get('/', function () {
     return Inertia::render('Welcome', [
@@ -24,8 +15,16 @@ Route::get('/', function () {
     ]);
 });
 
-Route::get('/dashboard', function () {
-    return Inertia::render('Dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
+
+
+
+Route::group(['middleware' => ['auth', 'verified']], function(){
+    Route::get('/dashboard', function () {
+        return Inertia::render('Dashboard');
+    })->name('dashboard');
+
+
+    Route::resource('/communities', CommunityController::class);
+});
 
 require __DIR__.'/auth.php';
